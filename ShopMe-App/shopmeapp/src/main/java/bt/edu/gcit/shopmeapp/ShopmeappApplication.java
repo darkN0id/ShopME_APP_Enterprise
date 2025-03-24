d0d2c078511753dedc8ad373c4f25ff66af62e13
darkN0id <12220098.gcit@rub.edu.bt>
2025-03-24 21:20:08 +0600

    added

---
+++ ShopMe-App/shopmeapp/src/main/java/bt/edu/gcit/shopmeapp/ShopmeappApplication.java
@@ -0,0 +1,13 @@
+package bt.edu.gcit.shopmeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopmeappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopmeappApplication.class, args);
+	}
+
+}
---
+++ ShopMe-App/shopmeapp/src/test/java/bt/edu/gcit/shopmeapp/ShopmeappApplicationTests.java
@@ -0,0 +1,13 @@
+package bt.edu.gcit.shopmeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopmeappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
